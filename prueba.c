57b7b4c31f23b39ba1393c9ad200924213772e58
MatiasAAguirre <aguirrematiasa@live.com.ar>
2019-03-25 19:08:58 -0300

    Cambios varios, arreglo de la mala asignación.

--- prueba.c
+++ prueba.c
@@ -7,28 +7,29 @@ int poblar (int *red, float p, int dim);
 int clasificar(int *red,int dim);
 int imprimir(int *red,int dim);
 
-int main(int argc,char** argv[])
+//Función principal:
+int main(int argc,char*argv[])
 {
   float p;
   int dim;
   int *red;
-  red = malloc(dim*dim*sizeof(int));    //Reserva el espacio necesario para la red.
   sscanf(argv[1],"%d",& dim);           //Busca el primero de los argumentos y lo usa como dim.
   sscanf(argv[2],"%f",& p);             //Busca el segundo de los argumentos y lo usa como p.
+  red = malloc(dim*dim*sizeof(int));    //Reserva el espacio necesario para la red.
 
-  poblar(red, p, dim);                  //Usa la funcion poblar.
+  poblar(red, p, dim);
+  imprimir(red,dim);
+  printf("\n");
   clasificar(red,dim);
   imprimir(red,dim);
   return 0;
 }
 
-
-
-
+//Funciones secundarias:
 int poblar(int *red, float p, int dim)
 {
   float random;
-  int i,j;
+  int i;
   srand(time(NULL));
 
 
@@ -40,106 +41,76 @@ int poblar(int *red, float p, int dim)
       *(red+i)=1;                       //Asigna como <lleno> si el valor aleatorio es menor.
     }
 
-    for (i=0;i<dim;i++) // Imprimo la red para ver como quedo
-      {for (j=0;j<dim;j++)
-        {
-        printf("%d ", *(red+dim*i+j));
-        }
-      printf("\n");
-      }
-
   return 0;
 }
+
 
 int clasificar(int *red,int dim)
 {
   int S1,S2,i,j;
   int frag = 2;
 
-  if(*(red)) //primer lugar
+  if(*(red)) //Primer lugar.
   { *(red) = frag;
     frag++;
   }
 
-
-
-
-
-  for(i=1;i<dim;i++) //primera fila sin primer lugar
+  for(i=1;i<dim;i++) //Primera fila sin primer lugar.
   { S1= *(red+i-1);
-    if(*(red+i) && S1)
+    if(*(red+i) && S1)  //S1 es 1.
     { *(red+i) = S1;
     }
-    if(*(red+i) && !S1)
-    {
-      *(red+i)=frag;
+    if(*(red+i) && !S1)  //S1 es 0.
+    { *(red+i)=frag;
       frag++;
     }
   }
 
-  for(i=1;i<dim;i++)  //primera columna sin primer lugar
+  for(i=1;i<dim;i++)  //Primera columna sin primer lugar.
   {
-    S2 = *(red+(i*dim-dim)); //ESTO. Aca teniamos un red+i-dim
-    if(*(red+(i*dim)) && S2)
-    {
-      *(red+(i*dim)) = S2;
+    S2 = *(red+(i*dim-dim));
+    if(*(red+(i*dim)) && S2)  //S2 es 1.
+    { *(red+(i*dim)) = S2;
     }
-    if(*(red+(i*dim)) && !S2)
-    {
-      *(red+(i*dim))=frag;
+    if(*(red+(i*dim)) && !S2)  //S2 es 0.
+    { *(red+(i*dim))=frag;
       frag++;
     }
   }
 
-
-  for(i=1;i<dim;i++) //todo el resto
+  for(i=1;i<dim;i++) //Todo el resto.
   {
     for(j=1;j<dim;j++)
     {
       S1 = *(red+(i*dim+j-1));
       S2 = *(red+(i*dim+j-dim));
 
-      if( *(red+(i*dim+j)) && (S1 || S2)) // Acá entran cuando los dos son 1 o cuando uno solo es 1. Los primeros tres if son si los dos son 1. Los otros dos si uno es 1.
-        {
-         if (S1 && S2 && (S1>S2)) //Los dos son 1 y S1 más grande
-          {
-            *(red+(i*dim+j)) = S2;
-          }
-         if (S1 && S2 && (S2>S1)) //Los dos son 1 y S2 más grande
-         {
-           *(red+(i*dim+j)) = S1;
-         }
-
-         if (S1 && S2 && (S2=S1)) //Los dos son 1 y son iguales, elijo arbitrariamente S2
-         {
-           *(red+(i*dim+j)) = S2;
-         }
-         }
-
-
-         if ((!S1) && S2) //S1 es cero y S2 es 1
-        {
-          *(red+(i*dim+j))=S2;
-
+      if( *(red+(i*dim+j)) && (S1 || S2)) // Acá entran cuando los dos o uno de ellos son 1.
+      { if (S1 && S2 && (S2<S1)) //Los dos son 1 y S1 más grande.
+        { *(red+(i*dim+j))=S2;
         }
-
-        if (S1 && (!S2)) //S1 es 1 y S2 es cero
-       {
-         *(red+(i*dim+j))=S1;
-
+        if (S1 && S2 && (S1<S2)) //Los dos son 1 y S2 más grande.
+        { *(red+(i*dim+j))=S1;
+        }
+        if (S1 && S2 && (S2==S1)) //Los dos son 1 y son iguales, elijo arbitrariamente S2.
+        { *(red+(i*dim+j))=S2;
+        }
+        if ((!S1) && S2) //S1 es cero y S2 es 1.
+        { *(red+(i*dim+j))=S2;
+        }
+        if (S1 && (!S2)) //S1 es 1 y S2 es cero.
+        { *(red+(i*dim+j))=S1;
+        }
        }
 
-
-
-
-      if ( *(red+(i*dim+j)) && (!S1 && !S2)) //los dos son cero
-      {
-        *(red+(i*dim+j))=frag;
+      if ( *(red+(i*dim+j)) && (!S1 && !S2)) //los dos son cero.
+      { *(red+(i*dim+j))=frag;
         frag++;
       }
     }
 
   }
+
   return 0;
 
 }
