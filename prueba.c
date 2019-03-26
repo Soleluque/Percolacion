23caddba0874aac8f9a659ab87acb0adad4fa40f
Sol <sol@sol-HP-Laptop-15-bs0xx>
2019-03-26 19:25:46 -0300

    No sé si está mirando que pasa en la primera columna y la primera fila. El historial lo está cambiando, pero la red no.

--- prueba.c
+++ prueba.c
@@ -5,6 +5,7 @@
 
 int poblar (int *red, float p, int dim);
 int clasificar(int *red,int dim);
+int etiqueta_falsa(int *red, int *hist, int S1, int S2, int i,int j, int dim);
 int imprimir(int *red,int dim);
 
 int main(int argc,char** argv[])
@@ -12,6 +13,7 @@ int main(int argc,char** argv[])
   float p;
   int dim;
   int *red;
+
   red = malloc(dim*dim*sizeof(int));    //Reserva el espacio necesario para la red.
   sscanf(argv[1],"%d",& dim);           //Busca el primero de los argumentos y lo usa como dim.
   sscanf(argv[2],"%f",& p);             //Busca el segundo de los argumentos y lo usa como p.
@@ -19,6 +21,9 @@ int main(int argc,char** argv[])
   poblar(red, p, dim);                  //Usa la funcion poblar.
   clasificar(red,dim);
   imprimir(red,dim);
+  free(red);
+
+
   return 0;
 }
 
@@ -47,7 +52,7 @@ int poblar(int *red, float p, int dim)
         }
       printf("\n");
       }
-
+      printf("\n");
   return 0;
 }
 
@@ -55,22 +60,39 @@ int clasificar(int *red,int dim)
 {
   int S1,S2,i,j;
   int frag = 2;
+  int *hist;
+  int minimo;
+  int maximo;
+
+  hist = malloc((dim*dim)/2);
+
+  for(i=0;i<=(dim*dim)/2;i++) //genero historial
+    {
+      *(hist+i)=i;
+    }
+
+
+
 
   if(*(red)) //primer lugar
-  { *(red) = frag;
-    frag++;
-  }
+    { *(red) = *(hist+frag);
+      frag++;
+
+    }
 
   for(i=1;i<dim;i++) //primera fila sin primer lugar
   { S1= *(red+i-1);
     if(*(red+i) && S1)
-    { *(red+i) = S1;
+    { *(red+i) = *(hist+S1);
+      etiqueta_falsa(red, hist, S1, S2, i,j,dim);
     }
     if(*(red+i) && !S1)
     {
-      *(red+i)=frag;
+      *(red+i)=*(hist+frag);
       frag++;
     }
+
+
   }
 
   for(i=1;i<dim;i++)  //primera columna sin primer lugar
@@ -78,14 +100,17 @@ int clasificar(int *red,int dim)
     S2 = *(red+(i*dim-dim)); //ESTO. Aca teniamos un red+i-dim
     if(*(red+(i*dim)) && S2)
     {
-      *(red+(i*dim)) = S2;
+      *(red+(i*dim)) = *(hist+S2);
+      etiqueta_falsa(red, hist, S1, S2, i,j,dim);
     }
     if(*(red+(i*dim)) && !S2)
     {
-      *(red+(i*dim))=frag;
+      *(red+(i*dim))=*(hist+frag);
       frag++;
     }
+
   }
+
 
 
   for(i=1;i<dim;i++) //todo el resto
@@ -99,50 +124,97 @@ int clasificar(int *red,int dim)
         {
          if (S1 && S2 && (S2<S1)) //Los dos son 1 y S1 más grande
           {
-            *(red+(i*dim+j)) = S2;
+            *(red+(i*dim+j)) = *(hist+S2);
+            *(hist+S1)=-*(hist+S2);
+            etiqueta_falsa(red, hist, S1, S2, i,j,dim);
           }
          if (S1 && S2 && (S1<S2)) //Los dos son 1 y S2 más grande
          {
-           *(red+(i*dim+j)) = S1;
+           *(red+(i*dim+j)) = *(hist+S1);
+           *(hist+S2)=-*(hist+S1);
+           etiqueta_falsa(red, hist, S1, S2, i,j,dim);
          }
 
-         if (S1 && S2 && (S2=S1)) //Los dos son 1 y son iguales, elijo arbitrariamente S2
+         if (S1 && S2 && (S2==S1)) //Los dos son 1 y son iguales, elijo arbitrariamente S2
          {
-           *(red+(i*dim+j)) = S2;
+           *(red+(i*dim+j)) = *(hist+S2);
+           etiqueta_falsa(red, hist, S1, S2, i,j,dim);
+
          }
         if ((!S1) && S2) //S1 es cero y S2 es 1
         {
-          *(red+(i*dim+j))=S2;
+          *(red+(i*dim+j))=*(hist+S2);
         }
         if (S1 && (!S2)) //S1 es 1 y S2 es cero
        {
-         *(red+(i*dim+j))=S1;
+         *(red+(i*dim+j))=*(hist+S1);
        }
        }
 
       if ( *(red+(i*dim+j)) && (!S1 && !S2)) //los dos son cero
       {
-        *(red+(i*dim+j))=frag;
+        *(red+(i*dim+j))=*(hist+frag);
         frag++;
       }
-    }
+
 
   }
-  return 0;
-
 }
 
 
-int imprimir(int *red, int dim)         //Imprime una fila debajo de la otra.
+
+  for (i=0;i<=(dim*dim)/2;i++) // Imprimo historial para ver
+    {
+    printf("%d\n",*(hist+i));
+    }
+    printf("\n");
+
+
+return 0;
+
+}
+
+int etiqueta_falsa(int *red, int *hist, int S1, int S2, int i,int j,int dim)
+
+{ int minimo, maximo;
+
+while(*(hist+S1)<0)
+{
+  S1=-*(hist+S1);
+}
+while(*(hist+S2)<0)
+{
+  S2=-*(hist+S2);
+}
+if(S1<S2)
+{
+  minimo=S1;
+  maximo=S2;
+}
+else
+{
+  minimo=S2;
+  maximo=S2;
+ }
+*(red+(i*dim+j))=minimo;
+*(hist+maximo)=-minimo;
+*(hist+minimo)=minimo;
+
+return 0;
+}
+
+int imprimir(int *red, int dim)
 { int i,j;
 
-  for (i=0;i<dim;i++)
-    {for (j=0;j<dim;j++)
-      {
-      printf("%d ", *(red+dim*i+j));
-      }
-    printf("\n");
-    }
 
-  return 0;
+for (i=0;i<dim;i++) // Imprimo la red para ver como quedo
+  {for (j=0;j<dim;j++)
+    {
+    printf("%d ", *(red+dim*i+j));
+    }
+  printf("\n");
+    }
+  printf("\n");
+
+return 0;
 }
