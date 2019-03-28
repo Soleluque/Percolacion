cf6ed9a5ca7756022165f459caf692c44cb882e2
Matias Aguirre <aguirrematiasa@live.com.ar>
2019-03-28 13:52:08 -0300

    Corregido etiqueta, agregado percolante.

--- prueba.c
+++ prueba.c
@@ -4,7 +4,9 @@
 #include <time.h>
 
 int poblar (int *red, float p, int dim);
+int etiqueta_falsa(int *red,int *historial, int S1, int S2, int i, int j, int dim);
 int clasificar(int *red,int dim);
+int percolacion(int *red, int dim);
 int imprimir(int *red,int dim);
 
 
@@ -22,6 +24,7 @@ int main(int argc,char*argv[])
   printf("\n");
   clasificar(red,dim);
   imprimir(red,dim);
+  percolacion(red, dim);
   free(red);
   return 0;
 }
@@ -58,17 +61,17 @@ int clasificar(int *red,int dim)
   frag = 2;
 
   if(*(red)) //Primer lugar.
-  { *(red) = *(historial+frag);
+  { *(red) =frag;
     frag++;
   }
 
   for(i=1;i<dim;i++) //Primera fila sin primer lugar.
   { S1= *(red+i-1);
     if(*(red+i) && S1)  //S1 es 1.
-    { *(red+i) = *(historial+S1);
+    { *(red+i) = S1;
     }
     if(*(red+i) && !S1)  //S1 es 0.
-    { *(red+i)=*(historial+frag);
+    { *(red+i)=frag;
       frag++;
     }
   }
@@ -76,49 +79,87 @@ int clasificar(int *red,int dim)
   for(i=1;i<dim;i++)  //Primera columna sin primer lugar.
   { S2 = *(red+(i*dim-dim));
     if(*(red+(i*dim)) && S2)  //S2 es 1.
-    { *(red+(i*dim)) = *(historial+S2);
+    { *(red+(i*dim)) = S2;
     }
     if(*(red+(i*dim)) && !S2)  //S2 es 0.
-    { *(red+(i*dim))=*(historial+frag);
+    { *(red+(i*dim))=frag;
       frag++;
     }
   }
 
   for(i=1;i<dim;i++) //Todo el resto.
   { for(j=1;j<dim;j++)
-    { S1 = *(red+(i*dim+j-1));
-      S2 = *(red+(i*dim+j-dim));
+    { S1 = *(red+(i*dim+j-1)); //El casillero de la izquierda.
+      S2 = *(red+(i*dim+j-dim)); //El casillero de arriba.
 
       if( *(red+(i*dim+j)) && (S1 || S2)) // Acá entran cuando los dos o uno de ellos son 1.
       { if (S1 && S2 && (S2<S1)) //Los dos son 1 y S1 más grande.
-        { *(red+(i*dim+j))=*(historial+S2);
+        { *(red+(i*dim+j))=S2;
+          *(red+(i*dim+j-1))=S2;
           *(historial+S1) = -*(historial+S2);
         }
         if (S1 && S2 && (S1<S2)) //Los dos son 1 y S2 más grande.
-        { *(red+(i*dim+j))=*(historial+S1);
+        { *(red+(i*dim+j))=S1;
+          *(red+(i*dim+j-dim))=S1;
           *(historial+S2) = -*(historial+S1);
         }
         if (S1 && S2 && (S2==S1)) //Los dos son 1 y son iguales, elijo arbitrariamente S2.
-        { *(red+(i*dim+j))=*(historial+S2);
+        { *(red+(i*dim+j))=S2;
         }
         if ((!S1) && S2) //S1 es cero y S2 es 1.
-        { *(red+(i*dim+j))=*(historial+S2);
+        { *(red+(i*dim+j))=S2;
         }
         if (S1 && (!S2)) //S1 es 1 y S2 es cero.
-        { *(red+(i*dim+j))=*(historial+S1);
+        { *(red+(i*dim+j))=S1;
         }
        }
 
       if ( *(red+(i*dim+j)) && ((!S1) && (!S2))) //los dos son cero.
-      { *(red+(i*dim+j))=*(historial+frag);
+      { *(red+(i*dim+j))=frag;
         frag++;
       }
     }
   }
+
+  /*for(i=0;i<=(dim*dim)/2;i++)
+  { printf("%d ", *(historial+i));
+  }
+  printf("\n");*/
+
+  for(i=1;i<dim;i++) //Todo el resto.
+  { for(j=1;j<dim;j++)
+    { S1 = *(red+(i*dim+j-1)); //El casillero de la izquierda.
+      S2 = *(red+(i*dim+j-dim)); //El casillero de arriba.
+
+      etiqueta_falsa(red, historial, S1, S2, i, j, dim);
+      }
+  }
+
+  /*for(i=0;i<dim*dim;i++)
+  { *(red+i) = *(historial+*(red+i));
+  }*/
+
+  /*for(i=0;i<=(dim*dim)/2;i++)
+  { printf("%d ", *(historial+i));
+  }
+  printf("\n");*/
+
   free(historial);
   return 0;
 
   }
+
+int etiqueta_falsa(int *red,int *historial, int S1, int S2, int i, int j, int dim)
+{ while(*(historial+S1)<0 && S1)  //Cambia etiquete en historial.
+  { *(historial+S1) = -*(historial+S1);
+  }
+  while (*(historial+S2)<0 && S2)
+  { *(historial+S2) = -*(historial+S2);  //Cambia etiquete en historial.
+  }
+
+  return 0;
+
+}
 
 
 int imprimir(int *red, int dim)         //Imprime una fila debajo de la otra.
@@ -133,4 +174,28 @@ int imprimir(int *red, int dim)         //Imprime una fila debajo de la otra.
     }
 
   return 0;
+}
+
+int percolacion(int *red, int dim)
+{ int i,j;
+  int *percolante_s,*percolante_i;
+
+  percolante_s = malloc(dim);
+  percolante_i = malloc(dim);
+
+  for(i=0;i<dim;i++)
+  { *(percolante_s+i) = *(red+i);
+    *(percolante_i+i) = *(red+dim*dim+i);
+  }
+
+  for(i=0;i<dim;i++)
+  { for(j=0;j<dim;j++)
+    { if (*(percolante_s+i) == *(percolante_i+j) && *(percolante_s+i))
+      { printf("He percolado!");
+      }
+    }
+  }
+
+  return 0;
+
 }
